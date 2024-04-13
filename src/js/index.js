880beb301ed2571df4e4bea8885605e350e7a4ce
NicolasMurtha <nickymurtha@gmail.com>
2024-04-12 23:40:11 -0300

    ft. botão mostrar mais/menos

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
 const botao = document.querySelector('.btn-mostrar-projetos')
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
 
+
+
 botao.addEventListener('click', () => {
   projetosInativos.forEach(projetosInativo => {
-    projetosInativo.classList.add('ativo')
-    botao.innerHTML = "Mostrar menos"
-  });
 
-
-  botao.addEventListener('click', () => {
-    projetosInativos.forEach(projetosInativo => {
+    if (projetosInativo.classList.contains('ativo')) {
       projetosInativo.classList.remove('ativo')
       botao.innerHTML = "Mostrar mais"
-    });
-  })
+    } else {
+      projetosInativo.classList.add('ativo')
+      botao.innerHTML = "Mostrar menos"
+    }
+  });
 })
-botao.classList.add("remover");
+
 
